f20c3861fe7d1f6bbbe6aab0bb04a6eefe874a10
Armenteros05 <sarmerod@myuax.com>
2023-11-09 12:06:12 +0100

    reajuste del código y codigo final

--- P2_gestionestudiantes.cpp
+++ P2_gestionestudiantes.cpp
@@ -4,20 +4,30 @@ using namespace std;
 class Estudiante{
 private:
     string nombre;
-    int edad;
+    int edad{};
     string grado;
 public:
-    void mostrar_info(){
-        cout<<"Nombre: "<<nombre<<endl;
-        cin>>nombre;
-        cout<<"Edad: "<<edad<<endl;
-        cin>>edad;
-        cout<<"Grado: "<<grado<<endl;
-        cin>>grado;
+    void mostrar_info() {
+        cout << "Ingresa el nombre del estudiante: ";
+        cin >> nombre;
+
+        cout << "Ingresa la edad del estudiante: ";
+        cin >> edad;
+
+        cout << "Ingresa el grado del estudiante: ";
+        cin >> grado;
+
+        cout << "Nombre: " << nombre << endl;
+        cout << "Edad: " << edad << " anos" << endl;
+        cout << "Grado: " << grado << endl;
     }
 };
-int main(){
-    Estudiante estudiante1;
-    estudiante1.mostrar_info();
+
+int main() {
+    Estudiante estudiante;
+
+    cout << "Por favor, proporciona la informacion del estudiante:" << endl;
+    estudiante.mostrar_info();
+
     return 0;
 }
