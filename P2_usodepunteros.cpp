be5c25e340e064ba1d73e3ed0bef55dc52bf3bdd
Armenteros05 <sarmerod@myuax.com>
2023-11-09 12:18:01 +0100

    todo el main y P2_usodepunteros.cpp finalizado

--- P2_usodepunteros.cpp
+++ P2_usodepunteros.cpp
@@ -5,3 +5,13 @@ void intercambiar(int* a, int* b) {
     *a = *b;
     *b = temp;
 }
+int main(){
+   int n1= 7;
+   int n2= 2;
+    std::cout<<"Los valores originales son:" <<n1<<" y "<<n2<<std::endl;
+    int *p1=&n1;
+    int *p2=&n2;
+    intercambiar(p1,p2);
+    std::cout<<"Los valores intercambiados son:" <<n1<<" y "<<n2<<std::endl;
+    return 0;
+}
