52961b93ab7c66bb6766f799e6b37864afc5d7e6
Armenteros05 <sarmerod@myuax.com>
2023-11-09 12:43:21 +0100

    codigo finalizado

--- P2_promedioestudiantes.cpp
+++ P2_promedioestudiantes.cpp
@@ -4,5 +4,17 @@ using namespace std;
 int main(){
     vector<int> calificaciones= {10,9,8,7,6,5,4,3,2,1};
     cout << "Las calificaciones son: " << endl;
+    for (int calificacion : calificaciones) {
+        cout << " " << calificacion;
+    }
+    cout << endl;
+    int suma=0;
+    for (int calificacion : calificaciones) {
+        suma+=calificacion;
+        
+    }
+    double promedio= suma/calificaciones.size();
+    cout<<"El promedio es: "<<promedio<<endl;
+    return 0;
 
 }
