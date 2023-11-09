83f9773922560aec9ecba51571cbafc281d7a5c1
Armenteros05 <sarmerod@myuax.com>
2023-11-09 12:40:44 +0100

    inicio del codigo

---
+++ P2_promedioestudiantes.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> calificaciones= {10,9,8,7,6,5,4,3,2,1};
+    cout << "Las calificaciones son: " << endl;
+
+}
