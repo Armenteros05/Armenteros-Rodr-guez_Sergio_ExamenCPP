d9b634fe15fc2b565ba7936a54d4035a85f9724c
Armenteros05 <sarmerod@myuax.com>
2023-11-09 11:48:00 +0100

    creacion de suma y fin de operacionesbasicas.cpp

---
+++ P2_operacionesbasicas.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"Ingrese el primer numero: ";
+cin>>a;
+cout<<"Ingrese el segundo numero: ";
+cin>>b;
+cout<<"La suma es: "<<a+b<<endl;
+
+}
