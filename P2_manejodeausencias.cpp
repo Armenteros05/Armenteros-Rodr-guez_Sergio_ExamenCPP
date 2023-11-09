a30be073b86d96c2f405fe5a441562b9813c1add
Armenteros05 <sarmerod@myuax.com>
2023-11-09 12:49:18 +0100

    creacion de la clase publica de asistencia

---
+++ P2_manejodeausencias.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class asistencia {
+public:
+    asistencia(string fecha, string estado) : fecha(fecha), estado(estado) {}
+    string fecha;
+    string estado;
