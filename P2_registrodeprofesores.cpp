d04ef0068e0dbc201afd47ed96a30ed501b28be5
Armenteros05 <sarmerod@myuax.com>
2023-11-09 13:13:55 +0100

    codigo completo tabulando

---
+++ P2_registrodeprofesores.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+class profesores{
+public:
+std::string nombre;
+    int edad{};
+    std::string grado;
+    std::string materia;
+    void mostrar_info() {
+        std::cout << "Ingresa el nombre del profesor: ";
+        std::cin >> nombre;
+
+        std::cout << "Ingresa la edad del profesor: ";
+        std::cin >> edad;
+
+        std::cout << "Ingresa el grado del profesor: ";
+        std::cin >> grado;
+
+        std::cout << "Nombre: " << nombre << std::endl;
+        std::cout << "Edad: " << edad << std::endl;
+        std::cout << "Grado: " << grado << std::endl;
+    }
+    void agregar_materia(){
+        std::cout<<"Ingrese las materias que imparte: ";
+        std::cin>>materia;
+    }
+    void mostrar_materias(){
+        std::cout<<"Las materias que imparte son: "<<std::endl;
+        std::cout<<materia<<std::endl;
+    }
+};
+int main(){
+    profesores profesor;
+
+    std::cout << "Por favor, proporciona la informacion del profesor:" << std::endl;
+    profesor.mostrar_info();
+    profesor.agregar_materia();
+    profesor.mostrar_materias();
+
+    return 0;
+}
